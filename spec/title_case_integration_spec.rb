ce62bb31f97788bc5fd04e94e12316bf0676fdf9
Noah Yasskin and Sky Rousse <sky@>
2016-06-29 10:02:31 -0700

    setup launchy

--- spec/title_case_integration_spec.rb
+++ spec/title_case_integration_spec.rb
@@ -6,7 +6,8 @@ set(:show_exceptions, false)
 describe('the title case path', {:type => :feature}) do
   it('processes the user entry and returns it title cased') do
     visit('/')
-    fill_in('title', :with => 'green eggs and ham')
+    save_and_open_page
+    fill_in('title', :with => 'blue eggs and ham')
     click_button('Send')
     expect(page).to have_content('Green Eggs and Ham')
   end
